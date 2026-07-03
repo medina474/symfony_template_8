b79117ba4c725597e33e7df741a9a46afa0b2c23
e.medina <e.medina@neotech.fr>
2026-07-03 14:42:18 +0200

    tweak: Hadolint DL3008 Pin versions in apt get install. Instead of `apt-get install <package>` use `apt-get install <package>=<version>` sqlfluff

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -27,7 +27,7 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 		composer require symfony/security-bundle --no-interaction
 		composer require symfony/redis-messenger symfony/mercure-bundle symfony/notifier --no-interaction
 		composer require symfony/uid --no-interaction
-		
+
 		# Remove the project install block from this script and the compose.yaml
 		sed -i '/^\t###> dunglas\/symfony-docker ###/,/^\t###< dunglas\/symfony-docker ###/d' frankenphp/docker-entrypoint.sh
 		sed -i '/###> dunglas\/symfony-docker ###/,/###< dunglas\/symfony-docker ###/d' compose.yaml
