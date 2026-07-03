7e7030b041b9cbc7cc7fa3df64520e9f03bd6ec1
e.medina <e.medina@neotech.fr>
2026-07-03 09:01:32 +0200

    composer: symfony/stimulus-bundle symfony/ux-turbo

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -20,6 +20,7 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 		composer config --json extra.symfony.allow-contrib true
 		composer require --dev symfony/maker-bundle symfony/debug-pack symfony/test-pack phpstan/phpstan-symfony --no-interaction
 		composer require symfony/twig-bundle symfony/asset symfony/asset-mapper --no-interaction
+		composer require symfony/stimulus-bundle symfony/ux-turbo --no-interaction
 
 		# Remove the project install block from this script and the compose.yaml
 		sed -i '/^\t###> dunglas\/symfony-docker ###/,/^\t###< dunglas\/symfony-docker ###/d' frankenphp/docker-entrypoint.sh
