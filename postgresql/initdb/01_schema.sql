43df75e8c6e33da6cb28c3c4654a13adb2a0923a
e.medina <e.medina@neotech.fr>
2026-07-03 17:53:59 +0200

    tweak: Keywords should not be used as identifiers

--- postgresql/initdb/01_schema.sql
+++ postgresql/initdb/01_schema.sql
@@ -1,7 +1,7 @@
 -- Symfony
 -- ----------------------------------------------------------------------------
 create table public.doctrine_migration_versions (
-    version varchar(191) not null,
+    "version" varchar(191) not null,
     executed_at timestamp(0) default null::timestamp without time zone null,
     execution_time int4 null,
     constraint doctrine_migration_versions_pkey primary key (version)
